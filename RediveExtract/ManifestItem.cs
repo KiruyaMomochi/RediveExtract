cc6e30914a5cf640f2b12ad22421883de1d3c074
KiruyaMomochi <65301509+KiruyaMomochi@users.noreply.github.com>
2020-10-31 22:57:17 +0800

    add missing files

---
+++ RediveExtract/ManifestItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// ReSharper disable StringLiteralTypo
+
+namespace RediveExtract
+{
+    public struct ManifestItem
+    {
+        public string Uri;
+        public string Md5;
+        public string Category;
+        public int Length;
+
+        private ManifestItem(string text)
+        {
+            var fields = text.Split(',');
+            if (fields.Length != 5)
+                throw new NotImplementedException();
+            (Uri, Md5, Category) = (fields[0], fields[1], fields[2]);
+            Length = int.Parse(fields[3]);
+        }
+
+        public static ManifestItem Parse(string text) => new ManifestItem(text);
+
+        public static IEnumerable<ManifestItem> ParseAll(string manifests) =>
+            manifests.Split().AsParallel().Where(x => x != "").Select(ManifestItem.Parse);
+    }
+}
