581e7c9b1928b8ac67ebf73dfcb606daeb8f24d7
KiruyaMomochi <65301509+KiruyaMomochi@users.noreply.github.com>
2020-11-02 23:51:13 +0800

    workflow update

--- Scripts/Export-RediveDB.ps1
+++ Scripts/Export-RediveDB.ps1
@@ -8,6 +8,7 @@ function Get-RedivePool {
 
     $HashPre = $Hash.Substring(0, 2)
     Invoke-WebRequest -Uri "https://img-pc.so-net.tw/dl/pool/AssetBundles/$HashPre/$Hash" -OutFile $OutFile
+    return $OutFile
 }
 
 $csv = Import-Csv .\manifest\masterdata_assetmanifest -Header Path, md5, Category, Length
@@ -15,11 +16,12 @@ $null = New-Item -ItemType Directory "db", "db/csv", "db/lines", "db/json" -Forc
 
 foreach ($item in $csv) {
     $db = $item.md5 + ".sqlite"
-
-    Get-RedivePool -Hash ($item.md5)
+    
+    $null = Get-RedivePool -Hash ($item.md5)
     python $PSScriptRoot/export_db.py $item.md5 $db
     $tables = sqlite3 $db "SELECT tbl_name FROM sqlite_master WHERE type='table' and tbl_name not like 'sqlite_%'"
     $tables | ForEach-Object {
+        Write-Host $_
         sqlite3 $db '.header on' '.mode csv' ".output db/csv/$_.csv" "select * from $_;"
         sqlite3 $db '.header on' '.mode json' ".output db/json/$_.json" "select * from $_;"
     }
