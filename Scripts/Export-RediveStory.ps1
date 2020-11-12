1d80cf008e18de2de35bddd3679fe31fc260e215
KiruyaMomochi <65301509+KiruyaMomochi@users.noreply.github.com>
2020-11-13 05:40:33 +0800

    Fetch stories

--- Scripts/Export-RediveStory.ps1
+++ Scripts/Export-RediveStory.ps1
@@ -1,13 +1,29 @@
 Import-Module $PSScriptRoot/Get-RedivePool.psm1
 
-$csv = Import-Csv .\manifest\storydata_assetmanifest -Header Path, md5, Category, Length
 $null = New-Item -ItemType Directory "storydata", "storydata/yaml", "storydata/json" -Force
 $storydata = $csv | Where-Object Path -Match 'storydata_\d+.unity3d'
 
-foreach ($item in $storydata) {
+$updated = git log -p -1 .\manifest\storydata_assetmanifest | Select-String '^\+(.*)' | % {$_.Matches.Groups[1].Value} | ConvertFrom-Csv -Header Path, md5, Category, Length
+foreach ($item in $updated) {
     $id = [regex]::Match($item.Path, 'storydata_(\d+).unity3d').Groups[1].Value
     
+    Write-Host $id
+    
     $null = Get-RedivePool -Hash ($item.md5)
-    python $PSScriptRoot/export_storytext.py $item.md5 $id
-    dotnet run --project "$PSScriptRoot/../RediveExtract" --configuration Release -- deserialize --input $id --json "storydata/json/$id.json" --yaml "storydata/yaml/$id.yaml"
+    python $PSScriptRoot/export_storytext.py $item.md5 "$id.bytes"
+    dotnet run --project "$PSScriptRoot/../RediveExtract" --configuration Release -- deserialize --input "$id.bytes" --json "storydata/json/$id.json" --yaml "storydata/yaml/$id.yaml"
+}
+
+$all = Import-Csv .\manifest\storydata_assetmanifest -Header Path, md5, Category, Length
+foreach ($item in $all) {
+    $id = [regex]::Match($item.Path, 'storydata_(\d+).unity3d').Groups[1].Value
+    if (Get-Item "storydata/*/$id.*") {
+        continue
+    }
+
+    Write-Host $id
+    
+    $null = Get-RedivePool -Hash ($item.md5)
+    python $PSScriptRoot/export_storytext.py $item.md5 "$id.bytes"
+    dotnet run --project "$PSScriptRoot/../RediveExtract" --configuration Release -- deserialize --input "$id.bytes" --json "storydata/json/$id.json" --yaml "storydata/yaml/$id.yaml"
 }
