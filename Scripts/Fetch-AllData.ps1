e1eff90e52c29174685244d094b09b6ce1b135ac
KiruyaMomochi <65301509+KiruyaMomochi@users.noreply.github.com>
2020-11-16 04:24:03 +0800

    use param path

--- Scripts/Fetch-AllData.ps1
+++ Scripts/Fetch-AllData.ps1
@@ -1,2 +1,5 @@
+param (
+    $ManifestPath = './manifest/'
+)
 Import-Module $PSScriptRoot/Save-RedivePool.psm1
-Get-ChildItem ./manifest/masterdata_assetmanifest | Get-ManifestItem | ForEach-Object { $_ } | ForEach-Object -Parallel {Import-Module $using:PSScriptRoot/Save-RedivePool.psm1; Save-ManifestItem $_}
+Get-ChildItem $ManifestPath | Get-ManifestItem | ForEach-Object { $_ } | ForEach-Object -Parallel {Import-Module $using:PSScriptRoot/Save-RedivePool.psm1; Save-ManifestItem $_}
