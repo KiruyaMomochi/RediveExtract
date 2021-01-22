3924a8a64dc41055d18db10e45a6e2e9d8e89689
KiruyaMomochi <65301509+KiruyaMomochi@users.noreply.github.com>
2021-01-23 03:26:07 +0800

    Also extract files in prefab

--- RediveExtract/Unity3d.cs
+++ RediveExtract/Unity3d.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Specialized;
-using System.CommandLine.Help;
 using System.IO;
 using System.Text.Json;
 using AssetStudio;
@@ -25,30 +24,12 @@ namespace RediveExtract
                     var id = value.asset.m_PathID;
                     var file = dic[id];
                     var savePath = Path.Combine(dest.FullName, internalPath ?? "unknown");
-                    var saveDir = Path.GetDirectoryName(savePath) ?? throw new InvalidOperationException();
-                    Directory.CreateDirectory(saveDir);
 
-                    switch (file)
+                    if (ExtractUnity3dAsset(file, savePath))
+                        Console.WriteLine(internalPath);
+                    else
                     {
-                        case Texture2D texture2D:
-                        {
-                            var bitmap = texture2D.ConvertToBitmap(true);
-                            bitmap.Save(savePath);
-                            break;
-                        }
-                        case TextAsset textAsset:
-                        {
-                            using var f = File.OpenWrite(savePath);
-                            f.Write(textAsset.m_Script);
-                            break;
-                        }
-                        case MonoBehaviour monoBehaviour:
-                        {
-                            using var f = File.OpenWrite(savePath);
-                            monoBehaviour.ToType();
-                            JsonSerializer.SerializeAsync(f, monoBehaviour.ToType(), Json.Options).Wait();
-                            break;
-                        }
+                        Console.Error.WriteLine($"Ignored {file?.GetType()}: {internalPath}");
                     }
                 }
                 catch (Exception e)
@@ -58,7 +39,70 @@ namespace RediveExtract
                 }
             }
         }
-        
+
+        private static bool ExtractUnity3dAsset(object file, string savePath, bool changeExtension = false)
+        {
+            var saveDir = Path.GetDirectoryName(savePath);
+            if (saveDir != null) Directory.CreateDirectory(saveDir);
+
+            switch (file)
+            {
+                case null:
+                    return false;
+                case Texture2D texture2D:
+                {
+                    if (changeExtension)
+                        savePath = Path.ChangeExtension(savePath, "png");
+                    var bitmap = texture2D.ConvertToBitmap(true);
+                    bitmap.Save(savePath);
+                    break;
+                }
+                case TextAsset textAsset:
+                {
+                    if (changeExtension)
+                        savePath = Path.ChangeExtension(savePath, "bytes");
+                    using var f = File.OpenWrite(savePath);
+                    f.Write(textAsset.m_Script);
+                    break;
+                }
+                case MonoBehaviour monoBehaviour:
+                {
+                    if (changeExtension)
+                        savePath = Path.ChangeExtension(savePath, "json");
+                    using var f = File.OpenWrite(savePath);
+                    JsonSerializer.SerializeAsync(f, monoBehaviour.ToType(), Json.Options).Wait();
+                    break;
+                }
+                case GameObject gameObject when savePath.EndsWith(".prefab"):
+                {
+                    var newPath = Path.ChangeExtension(savePath, null);
+                    var someOk = false;
+                    var mustAppendPathId = false;
+                    
+                    foreach (var component in gameObject.m_Components)
+                    {
+                        var f = gameObject.assetsFile.ObjectsDic[component.m_PathID];
+                        var sp = newPath;
+                        
+                        if (f is not MonoBehaviour || mustAppendPathId)
+                            sp = newPath + "_" + component.m_PathID;
+                        else
+                            mustAppendPathId = true;
+                        
+                        var res = ExtractUnity3dAsset(f, sp, true);;
+                        if (!res) Console.Error.WriteLine($"Ignored {f?.GetType()} :{component.m_PathID}");
+                        someOk |= res;
+                    }
+
+                    return someOk;
+                }
+                default:
+                    return false;
+            }
+
+            return true;
+        }
+
         private static byte[] ExtractTextAsset(TextAsset textAsset)
         {
             return textAsset.m_Script;
