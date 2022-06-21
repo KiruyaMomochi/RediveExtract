81e9b39a88d919436a9c37711adb565011335f80
Kiruya Momochi <65301509+KiruyaMomochi@users.noreply.github.com>
2022-06-22 06:38:03 +0800

    Use a single action to fetch everything
    Using multiple action doesn't decrease time consumed significantly,
    but cases push/pull racing between them.

--- scripts/update.ps1
+++ scripts/update.ps1
@@ -1,11 +1,53 @@
 #!/usr/bin/env pwsh
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory)][string]$Program
+)
 
-$Binary = /home/kyaru/KiruyaMomochi/RediveExtract/RediveExtract/bin/Release/net5.0/RediveExtract
+Import-Module (Join-Path $PSScriptRoot 'ngdive.psm1')
+
+<#
+.SYNOPSIS
+Use git to check if a file is updated or created.
+#>
+function FileUpdated {
+    git status $args[0] -s
+}
+
+& $Program fetch --step
+while (FileUpdated './manifest') {
+    if (FileUpdated './manifest/masterdata2_assetmanifest') {
+        Import-Manifest ./manifest/masterdata2_assetmanifest 
+        | Save-AssetItem -OutputDirectory . 
+        | Expand-AssetItem -Program $Program -OutputDirectory ./db
+    }
+
+    if (FileUpdated './manifest/storydata2_assetmanifest') {
+        Get-GitAddLines -Path ./manifest/storydata2_assetmanifest -Last 1
+        | ConvertFrom-Manifest
+        | Where-Object Path -Like '*storydata_[0-9]*'
+        | Save-AssetItem -OutputDirectory .
+        | Expand-AssetItem -Program $Program -OutputDirectory ./storydata
 
 
+        Get-GitAddLines -Path ./manifest/storydata2_assetmanifest -Last 1
+        | ConvertFrom-Manifest
+        | Where-Object Path -Like '*storydata_*movie_[0-9]*'
+        | Save-AssetItem -OutputDirectory .
+        | Expand-AssetItem -Program $Program -OutputDirectory ./subtitle
+    }
 
-pushd /home/kyaru/KiruyaMomochi/RediveExtract/ && git pull
-Import-Module /home/kyaru/KiruyaMomochi/RediveExtract/Scripts/ngdive.psm1
-Expand-NewAssets -Program $Binary -Commit (Get-Content /home/kyaru/KiruyaMomochi/.commit) -ManifestDirectory /home/kyaru/KiruyaMomochi/RediveData/manifest/ -AssetPath /home/kyaru/KiruyaMomochi/RawData/ -OutputDirectory /home/kyaru/KiruyaMomochi/ExtractedData/ > /home/kyaru/KiruyaMomochi/ExtractedData/updatelog; git rev-parse --verify HEAD > /home/kyaru/KiruyaMomochi/.commit
-popd
+    if (FileUpdated './manifest/consttext2_assetmanifest') {
+        Import-Manifest ./manifest/consttext2_assetmanifest
+        | Save-AssetItem -OutputDirectory '.'
+        | Expand-AssetItem -Program $Program -OutputDirectory ./consttext
+    }
 
+    Invoke-RediveCommit -Type Manifest
+    Invoke-RediveCommit -Type Database
+    Invoke-RediveCommit -Type StoryData
+    Invoke-RediveCommit -Type Subtitle
+    Invoke-RediveCommit -Type ConstText
+
+    & $Program fetch --step
+}
